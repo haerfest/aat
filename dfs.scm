e0829a93051e4ae074eb74ea606f07aa663cde06
Haerfest <wouter.hobers+git@gmail.com>
2022-12-18 17:20:27 +0100

    Parse DFS catalog.

--- dfs.scm
+++ dfs.scm
@@ -11,6 +11,7 @@
     (chicken pathname)
     (chicken port)
     coops
+    list-comprehensions
     scheme)
 
   ;; https://sweh.spuddy.org/Beeb/mmb_utils.html
@@ -22,6 +23,37 @@
      (sector-count initform: 0 accessor: sector-count)
      (files initform: '() accessor: files)))
 
+  (define (parse-filenames-and-dirs bitstr files)
+    (when (pair? files)
+      (bitmatch bitstr 
+        (((Filename (* 8 7) bitstring)
+          (Locked? 1)
+          (Directory 7)
+          (Remainder bitstring))
+         (let ((file (car files)))
+          (set-attribute! 'filename (bitstring->string Filename) file)
+          (set-attribute! 'directory (integer->char Directory) file)
+          (set-attribute! 'locked? (= 1 Locked?) file)
+          (parse-filenames-and-dirs Remainder (cdr files)))))))
+  
+  (define (parse-file-attributes bitstr files)
+    (if (pair? files)
+      (bitmatch bitstr 
+        (((LoadAddr 16 little unsigned)
+          (ExecAddr 16 little unsigned)
+          (Size 16 little unsigned)
+          (ExecAddrHigh 2)
+          (SizeHigh 2)
+          (LoadAddrHigh 2)
+          (StartSector 10 big unsigned)
+          (Remainder bitstring))
+         (let ((file (car files)))
+          (set-attribute! 'load-addr (+ (* 65536 LoadAddrHigh) LoadAddr) file)
+          (set-attribute! 'exec-addr (+ (* 65536 ExecAddrHigh) ExecAddr) file)
+          (set-attribute! 'size (+ (* 65536 SizeHigh) Size) file)
+          (set-attribute! 'start-sector StartSector file)
+          (parse-file-attributes Remainder (cdr files)))))))
+
   (define (parse-content content dfs)
     (bitmatch (substring content 0 (* 2 256))
       (((DiskTitleFirst8 (* 8 8) bitstring)
@@ -29,11 +61,10 @@
         (DiskTitleLast4 (* 4 8) bitstring)
         (WriteCycleCount 8 unsigned)
         (FileCountTimes8 8 unsigned)
-        (Ignored1 2)
+        (_ 2)
         (Opt4 2)
-        (Ignored2 2)
-        (SectorCountHigh 2)
-        (SectorCountLow 8 unsigned)
+        (_ 2)
+        (SectorCount 10 big unsigned)
         (FileAttributes (* 31 8 8) bitstring))
        (begin
         (set! (title dfs)
@@ -42,8 +73,11 @@
             (bitstring->string DiskTitleLast4)))
         (set! (write-cycle-count dfs) WriteCycleCount)
         (set! (opt-4 dfs) Opt4)
-        (set! (sector-count dfs) (+ (* SectorCountHigh 256) SectorCountLow))
-        dfs))))
+        (set! (sector-count dfs) SectorCount)
+        (let ((fls (map (lambda (_) (make <file>)) (range (/ FileCountTimes8 8)))))
+            (parse-filenames-and-dirs FilenamesAndDirs fls)
+            (parse-file-attributes FileAttributes fls)
+            (set! (files dfs) fls))))))
 
   (define-method (open-port (port #t) (archive <dfs>))
     (parse-content (read-string #f port) archive))
--- main.scm
+++ main.scm
@@ -1,5 +1,5 @@
-(load "dfs")
 (load "file")
+(load "dfs")
 (load "uef")
 
 (import
@@ -12,6 +12,10 @@
 (define (main)
   (let ((archive (make <dfs>)))
     (open-file "media/Elite.ssd" archive)
-    (format #t "Title: ~A~%Sectors: ~A~%" (title archive) (sector-count archive))))
+    (format #t "Title: ~A~%Sectors: ~A~%" (title archive) (sector-count archive))
+    (for-each
+      (lambda (file)
+        (format #t "~A~%" (fmt file)))
+      (files archive))))
 
 (main)
