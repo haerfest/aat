bfc3e6ea2b79264c560998ed25727b367d3b8c2f
Haerfest <wouter.hobers+git@gmail.com>
2022-12-22 13:25:52 +0100

    Refactoring.

--- main.scm
+++ main.scm
@@ -8,34 +8,46 @@
 (load "file")
 (load "fs")
 (load "dfs")
+(load "uef")
 
 (import (aat file))
 (import (aat fs))
 (import (aat dfs))
+(import (aat uef))
 
-;; =============================================================================
+;; -----------------------------------------------------------------------------
 
-(define (main)
-  (with-input-from-file "media/Elite.ssd"
-    (lambda ()
+(define (print-file file)
+  (format #t "~A\t&~X\t&~X\t~A\t~C~%"
+    (filename  file)
+    (load-addr file)
+    (exec-addr file)
+    (size      file)
+    (if (locked? file) #\L #\space)))
+
+(define (test-dfs)
+  (call-with-input-file "media/Elite.ssd"
+    (lambda (port)
       (let ((disc (make <dfs>)))
-        (mount disc (current-input-port))
+        (mount disc port)
         (format #t "~S (~A)  *OPT4,~A  ~A sectors~%"
           (title             disc)
           (write-cycle-count disc)
           (opt-4             disc)
           (sector-count      disc))
-        (for-each
-          (lambda (file)
-            (format #t "~A\t&~X\t&~X\t~A\t~C~%"
-              (filename  file)
-              (load-addr file)
-              (exec-addr file)
-              (size      file)
-              (if (locked? file) #\L #\space)))
-          (members disc))
+        (for-each print-file (members disc))
         (unmount disc)))))
 
-;; =============================================================================
+(define (test-uef)
+  (call-with-input-file "media/Hopper_E.uef"
+    (lambda (port)
+      (let ((tape (make <uef>)))
+        (mount tape port)
+        (for-each print-file (members tape))
+        (unmount tape)))))
+
+(define (main)
+  (test-dfs)
+  (test-uef))
 
 (main)
--- uef.scm
+++ uef.scm
@@ -7,57 +7,74 @@
 ;; -----------------------------------------------------------------------------
 
 (module (aat uef)
-  (<uef> read-port members)
+  (<uef> mount unmount members)
 
   (import
-    (aat archive)
     (aat file)
+    (aat fs)
     bitstring
     (chicken base)
     (chicken format)
     (chicken io)
     coops
+    coops-primitive-objects
     scheme
-    srfi-1)
+    srfi-1
+    srfi-13)
 
-  (define-class <uef> (<archive>))
+  (define-class <uef> (<fs>)
+    ((version initform: '() accessor: version)))
 
-  (define-method (read-port (tape <uef>) (port #t))
-    (set! (contents tape) (read-string #f port))
-    (parse-contents tape))
+  (define-method (mount (tape <uef>) (port <port>))
+    (parse tape (read-string #f port)))
+
+  (define-method (unmount (tape <uef>))
+    #f)
 
 ;; -----------------------------------------------------------------------------
 
-  (define-method (parse-contents (tape <uef>))
-    (bitmatch (contents tape)
+  ; https://daviddavidson.website/threading-macros-scheme/
+  (define-syntax ->>
+    (syntax-rules ()
+      ((->> value)
+        value)
+      ((->> value (fn args ...) rest ...)
+        (->> (fn args ... value) rest ...))
+      ((->> value fn rest ...)
+        (->> (fn value) rest ...))))
+
+  (define (parse tape bitstr)
+    (bitmatch bitstr
       ((("UEF File!")
         (#x00)
         (VersionMinor 8 unsigned)
         (VersionMajor 8 unsigned)
-        (Remaining bitstring))
+        (Remaining    bitstring))
        (begin
-        (set-meta! tape 'version (cons VersionMajor VersionMinor))
+        (set! (version tape) (cons VersionMajor VersionMinor))
         (set!
           (members tape)
-          (lambda ()
-            (blocks->files
-              (map parse-block (filter block? (parse-chunks Remaining))))))))))
+          (->> Remaining
+            parse-chunks
+            (filter block?)
+            (map parse-block)
+            blocks->files))))))
 
   (define-class <chunk> ()
-    ((id accessor: id)
-     (size accessor: size)
-     (data accessor: data)))
+    ((id       accessor: id)
+     (size     accessor: size)
+     (contents accessor: contents)))
 
   (define (parse-chunks bitstr #!optional (chunks '()))
     (bitmatch bitstr
-      (((Id 16 little unsigned)
-        (Size 32 little unsigned)
-        (Data (* 8 Size) bitstring)
+      (((Id        16 little unsigned)
+        (Size      32 little unsigned)
+        (Contents  (* 8 Size) bitstring)
         (Remaining bitstring))
        (let ((chunk (make <chunk>)))
-        (set! (id chunk) Id)
-        (set! (size chunk) Size)
-        (set! (data chunk) Data)
+        (set! (id chunk)       Id)
+        (set! (size chunk)     Size)
+        (set! (contents chunk) Contents)
         (parse-chunks Remaining (cons chunk chunks))))
       (else (reverse chunks))))
 
@@ -66,58 +83,61 @@
       (or (= (id chunk) #x0100)
           (= (id chunk) #x0102))
       (not (zero? (size chunk)))
-      (let ((first-byte (string-ref (bitstring->string (data chunk)) 0)))
+      (let ((first-byte (string-ref (bitstring->string (contents chunk)) 0)))
         (eq? #\* first-byte))))
 
   (define-class <block> ()
-    ((filename accessor: filename)
-     (load-addr accessor: load-addr)
-     (exec-addr accessor: exec-addr)
-     (number accessor: number)
-     (size accessor: size)
-     (last? accessor: last?)
-     (locked? accessor: locked?)
-     (header-crc accessor: header-crc)
-     (data accessor: data)
-     (data-crc accessor: data-crc)))
+    ((filename     accessor: filename)
+     (load-addr    accessor: load-addr)
+     (exec-addr    accessor: exec-addr)
+     (number       accessor: number)
+     (size         accessor: size)
+     (last?        accessor: last?)
+     (locked?      accessor: locked?)
+     (header-crc   accessor: header-crc)
+     (contents     accessor: contents)
+     (contents-crc accessor: contents-crc)))
 
   (define (parse-block chunk)
-    (let* ((bitstr (data chunk))
-           (n (- (list-index zero? (take (bitstring->list bitstr 8) 12)) 1)))
+    (let* ((bitstr (contents chunk))
+           (index  (list-index zero? (take (bitstring->list bitstr 8) 12)))
+           (n      (- index 1)))
       (bitmatch bitstr
         (((#\*)
-          (Filename (* 8 n) bitstring)
+          (Filename        (* 8 n) bitstring)
           (#x00)
-          (LoadAddr 32 little unsigned)
-          (ExecAddr 32 little unsigned)
-          (Number 16 little unsigned)
-          (Size 16 little unsigned)
-          (Last? 1) (_ 6) (Locked? 1)
+          (LoadAddr        32 little unsigned)
+          (ExecAddr        32 little unsigned)
+          (Number          16 little unsigned)
+          (Size            16 little unsigned)
+          (Last?           1) (_ 6) (Locked? 1)
           (AddressNextFile 32 little unsigned)
-          (HeaderCrc 16 little unsigned)
-          (DataAndCrc bitstring))
+          (HeaderCrc       16 little unsigned)
+          (ContentsAndCrc  bitstring))
          (let ((block (make <block>)))
-          (set! (filename block) (bitstring->string Filename))
-          (set! (load-addr block) LoadAddr)
-          (set! (exec-addr block) ExecAddr)
-          (set! (number block) Number)
-          (set! (size block) Size)
-          (set! (last? block) (= Last? 1))
-          (set! (locked? block) (= Locked? 1))
+          (set! (filename   block) (string-trim-right
+                                    (bitstring->string Filename)))
+          (set! (load-addr  block) LoadAddr)
+          (set! (exec-addr  block) ExecAddr)
+          (set! (number     block) Number)
+          (set! (size       block) Size)
+          (set! (last?      block) (= Last? 1))
+          (set! (locked?    block) (= Locked? 1))
           (set! (header-crc block) HeaderCrc)
-          (if (zero? Size)
-            (begin
-              (set! (data block) (string->bitstring ""))
-              (set! (data-crc block) 0))
-            (bitmatch DataAndCrc
-              (((Data (* 8 Size) bitstring)
-                (DataCrc 16 little unsigned))
+          (if (not (zero? Size))
+            (bitmatch ContentsAndCrc
+              (((Contents    (* 8 Size) bitstring)
+                (ContentsCrc 16 little unsigned))
                (begin
-                (set! (data block) Data)
-                (set! (data-crc block) DataCrc)))))
+                (set! (contents     block) Contents)
+                (set! (contents-crc block) ContentsCrc))))
+            (begin
+              (set! (contents     block) (string->bitstring ""))
+              (set! (contents-crc block) 0)))
           block)))))
 
-  (define (blocks->files blocks #!optional (files '()) (file #f) (expected-nr 0))
+  (define (blocks->files blocks
+                         #!optional (files '()) (file #f) (expected-nr 0))
     (if (null? blocks)
       (reverse files)
       (let ((block     (car blocks))
@@ -126,24 +146,26 @@
           ; finished any previous file, and start of a new file?
           ((and (not file) (= (number block) 0))
             (let ((file (make <file>)))
-              (set-meta! file 'filename  (filename block))
-              (set-meta! file 'load-addr (load-addr block))
-              (set-meta! file 'exec-addr (exec-addr block))
-              (set-meta! file 'locked?   (locked? block))
-              (set! (contents file) (data block))
+              (set! (filename  file) (filename  block))
+              (set! (load-addr file) (load-addr block))
+              (set! (exec-addr file) (exec-addr block))
+              (set! (locked?   file) (locked?   block))
+              (set! (contents  file) (contents  block))
               (if (last? block)
                 (begin
-                  (set-meta! file 'size (/ (bitstring-length (contents file)) 8))
+                  (set! (size file) (/ (bitstring-length (contents file)) 8))
                   (blocks->files remaining (cons file files)))
                 (blocks->files remaining files file 1))))
           ; continuing a file, and expected next block number?
           ((and file
-                (string=? (filename block) (get-meta file 'filename))
+                (string=? (filename block) (filename file))
                 (= (number block) expected-nr))
-           (set! (contents file) (bitstring-append (contents file) (data block)))
+           (set!
+            (contents file)
+            (bitstring-append (contents file) (contents block)))
            (if (last? block)
             (begin
-              (set-meta! file 'size (/ (bitstring-length (contents file)) 8))
+              (set! (size file) (/ (bitstring-length (contents file)) 8))
               (blocks->files remaining (cons file files)))
             (blocks->files remaining files file (+ expected-nr 1))))
           ; neither, skip this block
